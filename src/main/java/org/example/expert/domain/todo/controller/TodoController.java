221bce742e762a2daa3e077666b66e6f195cf66b
sjMun09 <ohoh7391@naver.com>
2025-03-14 11:58:57 +0900

    feat: 과제 3 완료.

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
@@ -54,5 +55,17 @@ public class TodoController {
                 "message", ex.getMessage()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    // 검색
+    @GetMapping("/todos/search")
+    public ResponseEntity<Page<TodoResponse>> searchTodos(
+            @RequestParam(required = false) String weather,
+            @RequestParam(required = false) LocalDateTime startDate,
+            @RequestParam(required = false) LocalDateTime endDate,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(todoService.searchTodos(weather, startDate, endDate, page, size));
     }
 }
